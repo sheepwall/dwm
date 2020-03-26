66a1bb8baae0f90dcf7e434a0578254dbc1f74ce
sheepwall <a.sve@live.se>
2020-03-26 15:37:02 +0100

    pedantics...

--- config.h
+++ config.h
@@ -8,7 +8,7 @@ static const unsigned int snap       = 32;       /* snap pixel */
 static const int showbar             = 1;        /* 0 means no bar */
 static const int topbar              = 1;        /* 0 means bottom bar */
 static const char *fonts[]           = { "monospace:size=14" };
-static const char dmenufont[]        = "monospace:size=10";
+static const char dmenufont[]        = "monospace:size=14";
 static char normbgcolor[]            = "#222222";
 static char normbordercolor[]        = "#444444";
 static char normfgcolor[]            = "#bbbbbb";
@@ -87,7 +87,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,                       XK_x,     xrdb,           {.v = NULL } },
+	{ MODKEY,                       XK_x,      xrdb,           {.v = NULL } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
