614d05f33981c2e3e6cbc587117131b141c502b1
sheepwall <a.sve@live.se>
2020-03-26 15:17:18 +0100

    Config is host-specific

--- config.h
+++ config.h
@@ -7,8 +7,8 @@ static const unsigned int gappx      = 10; 	/* gap pixel between windows */
 static const unsigned int snap       = 32;       /* snap pixel */
 static const int showbar             = 1;        /* 0 means no bar */
 static const int topbar              = 1;        /* 0 means bottom bar */
-static const char *fonts[]           = { "monospace:size=16" };
-static const char dmenufont[]        = "monospace:size=16";
+static const char *fonts[]           = { "monospace:size=14" };
+static const char dmenufont[]        = "monospace:size=10";
 static char normbgcolor[]            = "#222222";
 static char normbordercolor[]        = "#444444";
 static char normfgcolor[]            = "#bbbbbb";
